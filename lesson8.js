8472e1a3da5c39b92a5cefdfcb36f54fa03dc8d2
rahuljha661 <rahuljha12527@gmail.com>
2020-07-29 18:24:01 +0530

    type alias

---
+++ lesson8.js
@@ -0,0 +1,6 @@
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid of " + uid);
+};
+var great = function (user) {
+    console.log(user.name + " says hello");
+};
---
+++ lesson8.ts
@@ -0,0 +1,12 @@
+type StringOrNum=string|number;
+type objWithName={name:string,uid:StringOrNum}
+
+
+const logDetails=(uid:StringOrNum,item:string)=>{
+    console.log(`${item} has a uid of ${uid}`);
+}
+
+const great=(user:objWithName)=>{
+    console.log(`${user.name} says hello`);
+
+}
