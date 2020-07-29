df489ae4d87077b5d164bde168a1c0a7285c2eea
rahuljha661 <rahuljha12527@gmail.com>
2020-07-29 15:08:15 +0530

    lesson 4

---
+++ lesson3-array-object.js
@@ -0,0 +1,24 @@
+//arrays
+var names = ['lungi', 'mario', 'yashi'];
+names.push('toad');
+// names.push(3);
+//  names[0]=2;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1]='shaub';
+// objects
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age='32';
+// ninja.skills=['fighting','cooking'];
---
+++ lesson3-array-object.ts
@@ -0,0 +1,35 @@
+//arrays
+
+let names=['lungi','mario','yashi'];
+
+names.push('toad');
+// names.push(3);
+//  names[0]=2;
+
+
+let numbers=[10,20,30,40];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1]='shaub';
+// objects
+
+
+let mixed=['ken',4,'chun-li',8,9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0]=3;
+
+// objects
+let ninja={
+    name:'mario',
+    belt:'black',
+    age:30
+}
+
+ninja.age=40;
+ninja.name='ryu';
+// ninja.age='32';
+
+// ninja.skills=['fighting','cooking'];
+
