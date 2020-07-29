eeee31362d0d753e0e35d2a94a63eaf1337aeeb5
rahuljha661 <rahuljha12527@gmail.com>
2020-07-29 16:04:40 +0530

    lesson 5

--- lesson4.js
+++ lesson4.js
@@ -13,4 +13,12 @@ mixed.push('hello');
 mixed.push(20);
 mixed.push(false);
 console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+//uid=false
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
 // objects
---
+++ lesson5.ts
@@ -0,0 +1,27 @@
+let age:any=25;
+
+age=true;
+
+console.log(age);
+age="hello";
+console.log(age);
+
+age={name:'luigi'};
+console.log(age);
+
+
+
+
+let mixed:any[]=[];
+
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+
+let ninja:{name:any,age:any};
+ninja={name:'yoshi',age:24};
+
+console.log(ninja);
+
+
